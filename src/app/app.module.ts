830beab30aca5c15f1fe69b30045a21a0e7d0adc
Ella Kuksa <beeella2804@gmail.com>
2023-06-01 13:26:51 +0200

    Task 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { CourseCardComponent } from './components/courseCard/courseCard.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+import { HeaderComponent } from './components/header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { SearchBarComponent } from './components/searchBar/searchBar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CourseCardComponent,
+    BreadcrumbsComponent,
+    CoursesComponent,
+    SearchBarComponent,
+    FooterComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/models/course.ts
@@ -0,0 +1,7 @@
+export interface ICourse {
+  id: number;
+  title: string;
+  creationDate: string; // number | Date;
+  duration: number;
+  description: string;
+}
