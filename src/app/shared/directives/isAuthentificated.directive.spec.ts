6b84b74d31a83219e57f7f561ab7b3e831d014e7
Ella Kuksa <beeella2804@gmail.com>
2023-06-26 14:55:34 +0200

    Refactoring

--- src/app/shared/directives/isAuthentificated.directive.spec.ts
+++ src/app/shared/directives/isAuthentificated.directive.spec.ts
@@ -1,31 +1,13 @@
-import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 import { MockBuilder, MockRender } from 'ng-mocks';
+import { IsAuthenticatedDirective } from './ifAuthenticated.directive';
 
-@Directive({
-  selector: '[appAuthTestDirective]'
-})
-class IsAuthTestDirective {
-  public constructor(
-    protected templateRef: TemplateRef<any>,
-    protected viewContainerRef: ViewContainerRef
-  ) {}
-
-  @Input() public set appAuthTestDirective(value: any) {
-    if (value) {
-      this.viewContainerRef.createEmbeddedView(this.templateRef);
-    } else {
-      this.viewContainerRef.clear();
-    }
-  }
-}
-
-describe('IsAuthTestDirective', () => {
-  beforeEach(() => MockBuilder(IsAuthTestDirective));
+describe('IsAuthenticatedDirective', () => {
+  beforeEach(() => MockBuilder(IsAuthenticatedDirective));
 
   it('hides and renders its content', () => {
     const fixture = MockRender(
       `
-        <div *appAuthTestDirective="value">
+        <div *IsAuthenticatedDirective="value">
           content
         </div>
     `,
