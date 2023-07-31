b566a30da5c5e82ef9b024f1d1575c0e7bc2dad2
Ella Kuksa <beeella2804@gmail.com>
2023-07-31 17:54:13 +0200

    Task 10

--- src/app/store/effects/auth.effects.ts
+++ src/app/store/effects/auth.effects.ts
@@ -1,18 +1,33 @@
 import { Injectable } from '@angular/core';
 
-import { Effect, ofType, Actions } from '@ngrx/effects';
+import { createEffect, ofType, Actions } from '@ngrx/effects';
 import { Store, select } from '@ngrx/store';
 import { of } from 'rxjs';
 import { switchMap, map, withLatestFrom } from 'rxjs/operators';
 
 import { IAppState } from '../state/app.state';
-import { LoginUser, LogoutUser, GetAuth } from '../actions/auth.actions';
+import {
+  LoginUser,
+  LogoutUser,
+  GetAuth,
+  EAuthUserActions
+} from '../actions/auth.actions';
 import { AuthService } from 'src/app/core/services/auth.service';
 
-@Injectable()
+// @Injectable()
 export class AuthEffects {
-  @Effect()
-  getAuth$ = this._actions.pipe(ofType<>());
+  getAuth$ = createEffect(
+    () => {
+      this._actions.pipe(ofType<GetAuth>(EAuthUserActions.GetAuth)),
+        // withLatestFrom(this.store.select())
+        switchMap(() => this._authService.isAuthenticated());
+    },
+    { dispatch: false }
+  );
 
-  constructor(private _authService: AuthService, private _actions: Actions) {}
+  constructor(
+    private _authService: AuthService,
+    private _actions: Actions,
+    private store: Store
+  ) {}
 }
